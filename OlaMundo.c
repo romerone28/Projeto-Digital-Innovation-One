4649e6e34770744af007cc2b110965f8b3d2c26a
romerone28 <96501111+romerone28@users.noreply.github.com>
2021-12-21 20:12:56 -0300

    Add files via upload

---
+++ OlaMundo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main (void)//função principal
+{//início
+
+printf ("ola mundo!!!\n");
+
+system ("pause");
+
+return 0;
+
+
+}//fim do programa
---
+++ av1.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main(){
+	
+int a, b;
+int x=10, y=20;
+
+a = x++;
+b = ++y;
+
+printf ("%d e %d", a, b);
+
+return 0;	
+	
+	
+	
+	
+	
+}
